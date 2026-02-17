466b3982de20923db188e2f362be6fe99463bb72
mar6490 <141142611+mar6490@users.noreply.github.com>
2026-02-17 17:50:01 +0100

    Implement Block 3 fit-day gating and fit series artifacts

---
+++ src/pv_profiler/__init__.py
@@ -0,0 +1,19 @@
+"""pv_profiler package."""
+
+from .pipeline import (
+    run_block1_input_loader,
+    run_block2_sdt_from_csv,
+    run_block2_sdt_from_df,
+    run_block2_sdt_from_parquet,
+    run_block3_from_files,
+    run_single,
+)
+
+__all__ = [
+    "run_single",
+    "run_block1_input_loader",
+    "run_block2_sdt_from_df",
+    "run_block2_sdt_from_parquet",
+    "run_block2_sdt_from_csv",
+    "run_block3_from_files",
+]
